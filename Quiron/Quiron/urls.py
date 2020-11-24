534d38fd5e3726704c0caa89beaa54a262575476
Cesar Navarro <cesar.navarro.94@gmail.com>
2020-11-24 18:37:22 -0500

    User model created, Pillow installed to use pictures

--- Quiron/Quiron/urls.py
+++ Quiron/Quiron/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 """ Quiron apps views """
 from users import views as users_views
 
+""" Url patterns """
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    #path('profile/', views.user_profile, name = 'profile'),
 ]
--- Quiron/users/models.py
+++ Quiron/users/models.py
@@ -1,3 +1,23 @@
+""" Djanog models """
 from django.db import models
 
-# Create your models here.
+""" Import the User model from django to extend """
+
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    """ Profile models
+
+    Proxy model extends the base data with other information.
+    """
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+
+    picture = models.ImageField(upload_to='users/pictures', blank=True, null=True)
+    prof_register = models.CharField(max_length=20)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    """ Return the user username """
+    def __str__(self):
+        return self.user.username
