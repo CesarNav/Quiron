c7d89763fccd90080e6a92f02425eca3aae01017
Cesar Navarro <cesar.navarro.94@gmail.com>
2020-11-26 08:31:14 -0500

    Base template created and added to settings path

--- Quiron/Quiron/urls.py
+++ Quiron/Quiron/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from django.urls import path
 
 """ Quiron apps views """
-from users import views as users_views
+import users.views
 
 """ Url patterns """
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', users.views.user_home, name ='home'),
 ]
--- Quiron/users/views.py
+++ Quiron/users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 
+def user_home(request):
+    return render(request,'base.html')
+
 def user_profile(request):
     return render(request, '')
