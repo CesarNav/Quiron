0546e571b237a316158143dde3adec783dbc0dc8
Cesar Navarro <cesar.navarro.94@gmail.com>
2020-12-10 18:53:12 -0500

    Requirements added

--- Quiron/Quiron/settings.py
+++ Quiron/Quiron/settings.py
@@ -132,15 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 LOGIN_URL = 'users/login/'
 
-#Configure Django App for Heroku
-
+# Configure Django App for Heroku
 import django_heroku
 django_heroku.settings(locals())
